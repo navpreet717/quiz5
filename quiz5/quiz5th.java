89779c2dc8c51a3c4a09c7ab0d36ba4c4a33b345
748674 <748674@123-PC17.student.Tor.Loc>
2019-06-04 16:22:25 -0400

    R1

---
+++ quiz5/quiz5th.java
@@ -0,0 +1,27 @@
+package quiz5;
+
+import java.util.Scanner;
+
+public class quiz5th {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		Scanner keyboard=new Scanner(System.in);
+		System.out.println("Enter name");
+		String name=keyboard.next();
+		System.out.println(name+" is studying");
+		
+
+	}
+	public static String studying(String name){
+		
+		return name;
+		
+		
+	}
+		
+	
+	
+}
+
